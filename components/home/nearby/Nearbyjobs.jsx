ef256e02fd2333816229452f3f07c238025982d0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-05 21:51:42 +0700

    update-NearByJobs

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -42,6 +42,31 @@ const Nearbyjobs = () => {
                         </Text>
                     </TouchableOpacity>
                 </View>
+
+                <View
+                    style={styles.cardsContainer}
+                >
+                    {isLoading ? (
+                        <ActivityIndicator
+                            size='large'
+                            color={COLORS.primary}
+                        />
+                    ) : error ? (
+                        <Text>Something went wrong</Text>
+                    ) : (
+                        data?.map((job) => (
+                            <NearbyJobCard
+                                job={job}
+                                key={`nearby-job-${job.job_id}`}
+                                handleNavigate={
+                                    () => router
+                                    .push(`/job-details/${job.job_id}`)
+                                }
+                            />
+                        ))
+
+                    )}
+                </View>
             </View>
         </>
     );
