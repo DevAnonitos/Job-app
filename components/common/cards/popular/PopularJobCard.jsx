d6de6d9420eca6c52f27f952aebc1bbd1633b132
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2023-04-05 14:28:04 +0700

    update-Fix

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import styles from "./popularjobcard.style";
 
+import { checkImageURL } from "../../../../utils/index.js";
+
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
     return (
         <>
@@ -18,7 +20,9 @@ const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
                 >
                     <Image
                         source={{
-                            uri: item.employer_logo
+                            uri: checkImageURL(item?.employer_logo)
+                            ? item.employer_logo
+                            : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
                         }}
                         resizeMode='contain'
                         style={styles.logoImage}
